68560ebeef5f8bb597f1fff3d49bdb9d969d85e4
GrilloCha <charly.artulio001@gmail.com>
2023-10-20 18:48:16 -0600

    Se eficientó y comentó el codigo

--- ca1_ejercicio1.py
+++ ca1_ejercicio1.py
@@ -1,15 +1,16 @@
 from gpiozero import Button
-from time import sleep
 from datetime import datetime
 
+#Establecemos boton en el GPIO2
 button = Button(2)
 
-while True:
-    documentoBoton = open("botonRegistro.txt", "a")
-    fechaActual = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    if button.is_pressed:
-        data =  f"{fechaActual} --> Boton presionado"
-        documentoBoton.write(data)
-    documentoBoton.close()
-    
-    
+#Creamos y abrimos el documento
+with open("botonRegistro.txt", "a") as documentoBoton: 
+
+#Ciclo para comprobar estado del boton
+    while True:
+        fechaActual = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        if button.is_pressed:
+            data =  f"{fechaActual} --> Boton presionado\n"
+            documentoBoton.write(data)
+        
